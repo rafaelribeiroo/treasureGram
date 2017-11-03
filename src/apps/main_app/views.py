f6a449e53a03089821697e855710bcdc4dd79817
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2017-11-03 17:08:30 -0200

    PEP-8 followed, thank you Anaconda!

--- src/apps/main_app/views.py
+++ src/apps/main_app/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
-	name = 'Gold Nugget'
-	value = 1000.00
-	context = {
-		'treasure_name': name,
-		'treasure_value': value,
-	}
+    name = 'Gold Nugget'
+    value = 1000.00
+    context = {
+        'treasure_name': name,
+        'treasure_value': value,
+    }
 
-	return render(request, 'home.html', context)
+    return render(request, 'home.html', context)
