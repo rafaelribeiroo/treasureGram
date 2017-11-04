97daae878549a97c5fa48f72a81a4499faaf8043
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2017-11-04 12:34:42 -0200

    Working a lot

--- src/apps/main_app/models.py
+++ src/apps/main_app/models.py
@@ -1,7 +1,12 @@
+'''
+MakeMigrations: Makes a migration file
+Migrate: Applies the migration to the database
+Migrations are like a version control system for your database
+'''
 from django.db import models
 
 
-class Treasure(models.Model()):
+class Treasure(models.Model):
 
     name = models.CharField(max_length=100)
     value = models.DecimalField(max_digits=10,
@@ -9,3 +14,12 @@ class Treasure(models.Model()):
     material = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     img_url = models.CharField(max_length=100)
+
+    class Meta:
+            ordering = ['name']
+            verbose_name='treasure'
+            verbose_name_plural='treasures'
+
+    def __str__(self):
+    	return name
+
