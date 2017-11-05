2cee1aed943645f91dd5b131325ec2d63d7fef47
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2017-11-05 21:16:39 -0200

    Work

--- src/apps/main_app/urls.py
+++ src/apps/main_app/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls import url
 from .views import (
     home,
     detail,
+    post_treasure,
 )
 
 
 urlpatterns = [
     url(r'^$', home, name='homepage'),
     url(r'([0-9]+)/$', detail, name='detail'),
+    url(r'^post_url/$', post_treasure, name='post_treasure')
 ]
--- src/apps/main_app/views.py
+++ src/apps/main_app/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .models import Treasure
+from .forms import TreasureForm
 
 
 def home(request):
     treasures = Treasure.objects.all()
-    return render(request, 'home.html', {'treasures': treasures})
+    form = TreasureForm()
+    return render(request, 'home.html', {'form': form})
 
 
 def detail(request, treasure_id):
     treasure = Treasure.objects.get(pk=treasure_id)
     return render(request, 'detail.html', {'treasure': treasure})
+
+
+def post_treasure(request):
+    form = TreasureForm(request.POST)
+    if form.is_valid():
+        treasure = Treasure(name = form.cleaned_data['name'],
+                            value = form.cleaned_data['value'],
+                            material = form.cleaned_data['material'],
+                            location = form.cleaned_data['location'],
+                            img_url = form.cleaned_data['img_url'])
+    treasure.save()
+
+return HttpResponseRedirect('/')
+
